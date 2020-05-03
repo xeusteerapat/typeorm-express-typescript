facc89d8d782cbcd07dee975ce7c7ba60d7a0c8a
xeusteerapat <xeus085@gmail.com>
2020-05-03 12:57:00 +0700

    add update user by id

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,26 +1,36 @@
-import {getRepository} from "typeorm";
-import {NextFunction, Request, Response} from "express";
-import {User} from "../entity/User";
+import { getRepository } from 'typeorm';
+import { NextFunction, Request, Response } from 'express';
+import { User } from '../entity/User';
 
 export class UserController {
+  private userRepository = getRepository(User);
 
-    private userRepository = getRepository(User);
+  async all(request: Request, response: Response, next: NextFunction) {
+    return this.userRepository.find();
+  }
 
-    async all(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.find();
-    }
+  async one(request: Request, response: Response, next: NextFunction) {
+    return this.userRepository.findOne(request.params.id);
+  }
 
-    async one(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.findOne(request.params.id);
-    }
+  async save(request: Request, response: Response, next: NextFunction) {
+    return this.userRepository.save(request.body);
+  }
 
-    async save(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.save(request.body);
-    }
+  async update(request: Request, response: Response, next: NextFunction) {
+    const targetUser = new User();
+    targetUser.firstName = request.body.firstName;
+    targetUser.lastName = request.body.lastName;
+    targetUser.age = request.body.age;
 
-    async remove(request: Request, response: Response, next: NextFunction) {
-        let userToRemove = await this.userRepository.findOne(request.params.id);
-        await this.userRepository.remove(userToRemove);
-    }
+    await this.userRepository.update(request.params.id, targetUser);
+    const updatedUser = await this.userRepository.findOne(request.params.id);
+    response.send(updatedUser);
+  }
 
+  async remove(request: Request, response: Response, next: NextFunction) {
+    let userToRemove = await this.userRepository.findOne(request.params.id);
+    await this.userRepository.remove(userToRemove);
+    response.send('User has been deleted.');
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -42,20 +42,20 @@ createConnection()
     app.listen(8000);
 
     // insert new users for test
-    await connection.manager.save(
-      connection.manager.create(User, {
-        firstName: 'Timber',
-        lastName: 'Saw',
-        age: 27
-      })
-    );
-    await connection.manager.save(
-      connection.manager.create(User, {
-        firstName: 'Phantom',
-        lastName: 'Assassin',
-        age: 24
-      })
-    );
+    // await connection.manager.save(
+    //   connection.manager.create(User, {
+    //     firstName: 'Timber',
+    //     lastName: 'Saw',
+    //     age: 27
+    //   })
+    // );
+    // await connection.manager.save(
+    //   connection.manager.create(User, {
+    //     firstName: 'Phantom',
+    //     lastName: 'Assassin',
+    //     age: 24
+    //   })
+    // );
 
     console.log(
       'Express server has started on port 8000. Open http://localhost:8000/users to see results'
